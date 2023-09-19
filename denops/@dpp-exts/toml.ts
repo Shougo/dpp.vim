dacc2eb3d3c1b8274adbe7fff2b95936e4a60320
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-09-19 12:15:04 +0900

    Add toml ext

---
+++ denops/@dpp-exts/toml.ts
@@ -0,0 +1,53 @@
+import { Actions, BaseExt, Plugin } from "../dpp/types.ts";
+import { Denops } from "../dpp/deps.ts";
+import { parse } from "https://deno.land/std@0.118.0/encoding/toml.ts";
+import { basename } from "https://deno.land/std@0.201.0/path/mod.ts";
+
+type Params = Record<string, never>;
+
+type LoadArgs = {
+  path: string;
+  options?: Partial<Plugin>;
+};
+
+type Toml = {
+  hooks_file?: string;
+  plugins: Plugin[];
+};
+
+export class Ext extends BaseExt<Params> {
+  override actions: Actions<Params> = {
+    load: {
+      description: "Load toml config",
+      callback: async (args: {
+        denops: Denops;
+        actionParams: unknown;
+      }) => {
+        const params = args.actionParams as LoadArgs;
+        const path = await args.denops.call(
+          "dpp#util#_expand",
+          params.path,
+        ) as string;
+
+        const defaultOptions = params.options ?? {};
+
+        const toml = parse(await Deno.readTextFile(path)) as Toml;
+
+        const plugins = toml.plugins.map((plugin: Plugin) => {
+          return {
+            ...defaultOptions,
+            ...plugin,
+            name: plugin.name ?? basename(plugin.repo),
+          }
+        });
+        console.log(plugins);
+
+        return plugins;
+      },
+    },
+  };
+
+  override params(): Params {
+    return {};
+  }
+}
