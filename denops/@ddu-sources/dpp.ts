16492ad31dccdaa8a24f2a0643f7f77e9a4e3c6a
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-10-17 12:40:02 +0900

    Add ddu source

---
+++ denops/@ddu-sources/dpp.ts
@@ -0,0 +1,59 @@
+import {
+  ActionArguments,
+  ActionFlags,
+  BaseSource,
+  Item,
+} from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
+import { Denops, vars } from "https://deno.land/x/ddu_vim@v3.6.0/deps.ts";
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
+import { Plugin } from "../dpp/types.ts";
+
+type Params = Record<string, never>;
+
+type Action = {
+  path: string;
+  __name: string;
+};
+
+export class Source extends BaseSource<Params> {
+  override kind = "file";
+
+  override gather(args: {
+    denops: Denops;
+    sourceParams: Params;
+  }): ReadableStream<Item<ActionData>[]> {
+    return new ReadableStream({
+      async start(controller) {
+        const plugins = Object.values(
+          await vars.g.get(
+            args.denops,
+            "dpp#_plugins",
+          ),
+        ) as Plugin[];
+
+        const items = plugins.map((plugin) => {
+          return {
+            word: plugin.name,
+            action: {
+              path: plugin.path,
+              __name: plugin.name,
+            } as Action,
+          };
+        });
+
+        controller.enqueue(items);
+
+        controller.close();
+      },
+    });
+  }
+
+  override actions: Record<
+    string,
+    (args: ActionArguments<Params>) => Promise<ActionFlags>
+  > = {};
+
+  override params(): Params {
+    return {};
+  }
+}
