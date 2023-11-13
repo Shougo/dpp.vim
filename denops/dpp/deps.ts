625bad704f75bd67e237842fcd6e8a6ba6c19a38
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-11-13 18:19:44 +0900

    Update deps

--- denops/dpp/deps.ts
+++ denops/dpp/deps.ts
@@ -15,7 +15,7 @@ export { ensure, is } from "https://deno.land/x/unknownutil@v3.10.0/mod.ts";
 export {
   assertEquals,
   equal,
-} from "https://deno.land/std@0.205.0/assert/mod.ts";
+} from "https://deno.land/std@0.206.0/assert/mod.ts";
 export {
   basename,
   dirname,
@@ -24,12 +24,12 @@ export {
   parse,
   SEP as pathsep,
   toFileUrl,
-} from "https://deno.land/std@0.205.0/path/mod.ts";
+} from "https://deno.land/std@0.206.0/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.205.0/async/mod.ts";
+} from "https://deno.land/std@0.206.0/async/mod.ts";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
 export { Lock } from "https://deno.land/x/async@v2.0.2/mod.ts";
-export { deferred } from "https://deno.land/std@0.205.0/async/deferred.ts";
-export { copy } from "https://deno.land/std@0.205.0/fs/mod.ts";
+export { deferred } from "https://deno.land/std@0.206.0/async/deferred.ts";
+export { copy } from "https://deno.land/std@0.206.0/fs/mod.ts";
