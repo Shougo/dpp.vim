089cb192aaa512a8bd463f58d23cfe23a6929d21
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-11-21 20:30:52 +0900

    Add lua module

---
+++ lua/dpp.lua
@@ -0,0 +1,23 @@
+local M = setmetatable({}, {
+  __index = function(_, key)
+    return function(...)
+      -- NOTE: For keyword conflict
+      if vim.endswith(key, '_') then
+        key = key:sub(1, -2)
+      elseif key == 'load_state' then
+        key = 'min#load_state'
+      end
+
+      local ret = vim.call('dpp#' .. key, ...)
+
+      -- NOTE: For boolean functions
+      if type(ret) ~= 'table' and (vim.startswith(key, 'check_') or vim.startswith(key, 'is_')) then
+        ret = ret ~= 0
+      end
+
+      return ret
+    end
+  end,
+})
+
+return M
