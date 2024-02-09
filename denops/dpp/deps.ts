6a235e1def55919fd1c738b5171cd11cad11deba
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-02-09 13:09:13 +0900

    Update deps

--- denops/dpp/deps.ts
+++ denops/dpp/deps.ts
@@ -11,24 +11,24 @@ export * as op from "https://deno.land/x/denops_std@v6.0.1/option/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v6.0.1/function/mod.ts";
 export * as vars from "https://deno.land/x/denops_std@v6.0.1/variable/mod.ts";
 export * as autocmd from "https://deno.land/x/denops_std@v6.0.1/autocmd/mod.ts";
-export { ensure, is } from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
+export { ensure, is } from "https://deno.land/x/unknownutil@v3.15.0/mod.ts";
 export {
   assertEquals,
   assertInstanceOf,
   equal,
-} from "https://deno.land/std@0.214.0/assert/mod.ts";
+} from "https://deno.land/std@0.215.0/assert/mod.ts";
 export {
   basename,
   dirname,
   extname,
   join,
   parse,
-  SEP as pathsep,
+  SEPARATOR as pathsep,
   toFileUrl,
-} from "https://deno.land/std@0.214.0/path/mod.ts";
+} from "https://deno.land/std@0.215.0/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.214.0/async/mod.ts";
+} from "https://deno.land/std@0.215.0/async/mod.ts";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
 export { Lock } from "https://deno.land/x/async@v2.1.0/mod.ts";
