f9a4e6346ccf405cbb9809a62a9cdd9c2a36d279
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-03-01 11:26:06 +0900

    Update deps

--- denops/dpp/deps.ts
+++ denops/dpp/deps.ts
@@ -1,22 +1,22 @@
-export type { Denops } from "https://deno.land/x/denops_std@v6.1.0/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v6.2.0/mod.ts";
 export {
   echo,
   execute,
-} from "https://deno.land/x/denops_std@v6.1.0/helper/mod.ts";
+} from "https://deno.land/x/denops_std@v6.2.0/helper/mod.ts";
 export {
   batch,
   collect,
-} from "https://deno.land/x/denops_std@v6.1.0/batch/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v6.1.0/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v6.1.0/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v6.1.0/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v6.1.0/autocmd/mod.ts";
+} from "https://deno.land/x/denops_std@v6.2.0/batch/mod.ts";
+export * as op from "https://deno.land/x/denops_std@v6.2.0/option/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v6.2.0/function/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v6.2.0/variable/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v6.2.0/autocmd/mod.ts";
 export { ensure, is } from "https://deno.land/x/unknownutil@v3.16.3/mod.ts";
 export {
   assertEquals,
   assertInstanceOf,
   equal,
-} from "https://deno.land/std@0.217.0/assert/mod.ts";
+} from "https://deno.land/std@0.218.0/assert/mod.ts";
 export {
   basename,
   dirname,
@@ -25,10 +25,10 @@ export {
   parse,
   SEPARATOR as pathsep,
   toFileUrl,
-} from "https://deno.land/std@0.217.0/path/mod.ts";
+} from "https://deno.land/std@0.218.0/path/mod.ts";
 export {
   deadline,
   DeadlineError,
-} from "https://deno.land/std@0.217.0/async/mod.ts";
+} from "https://deno.land/std@0.218.0/async/mod.ts";
 export { TimeoutError } from "https://deno.land/x/msgpack_rpc@v4.0.1/response_waiter.ts";
 export { Lock } from "https://deno.land/x/async@v2.1.0/mod.ts";
