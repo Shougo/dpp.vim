8266f7ffe1060699ff49fbbc47f9726c42bbdbbd
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-07-27 13:40:56 +0900

    Use denops v7 pre

--- denops/dpp/deps.ts
+++ denops/dpp/deps.ts
@@ -1,21 +1,21 @@
 export type {
   Denops,
   Entrypoint,
-} from "https://deno.land/x/denops_std@v6.5.1/mod.ts";
+} from "jsr:@denops/std@7.0.0-pre2";
 export {
   echo,
   execute,
-} from "https://deno.land/x/denops_std@v6.5.1/helper/mod.ts";
+} from "jsr:@denops/std@7.0.0-pre2/helper";
 export {
   batch,
   collect,
-} from "https://deno.land/x/denops_std@v6.5.1/batch/mod.ts";
-export * as op from "https://deno.land/x/denops_std@v6.5.1/option/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v6.5.1/function/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v6.5.1/variable/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v6.5.1/autocmd/mod.ts";
+} from "jsr:@denops/std@7.0.0-pre2/batch";
+export * as op from "jsr:@denops/std@7.0.0-pre2/option";
+export * as fn from "jsr:@denops/std@7.0.0-pre2/function";
+export * as vars from "jsr:@denops/std@7.0.0-pre2/variable";
+export * as autocmd from "jsr:@denops/std@7.0.0-pre2/autocmd";
 
-export { assertEquals, assertInstanceOf, equal } from "jsr:@std/assert@1.0.0";
+export { assertEquals, assertInstanceOf, equal } from "jsr:@std/assert@1.0.1";
 export {
   basename,
   dirname,
@@ -24,7 +24,7 @@ export {
   parse,
   SEPARATOR as pathsep,
   toFileUrl,
-} from "jsr:@std/path@1.0.1";
+} from "jsr:@std/path@1.0.2";
 
 export { Lock } from "jsr:@lambdalisue/async@2.1.1";
 export { ensure, is } from "jsr:@core/unknownutil@3.18.1";
