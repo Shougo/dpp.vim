ed3f27904e1f109f6212f98acaacd6e9b94d873f
Shougo Matsushita <Shougo.Matsu@gmail.com>
2025-02-02 11:11:28 +0900

    Update deps

--- denops/@ddu-sources/dpp.ts
+++ denops/@ddu-sources/dpp.ts
@@ -7,8 +7,8 @@ import type {
   ActionArguments,
   ActionFlags,
   Item,
-} from "jsr:@shougo/ddu-vim@9.4.0/types";
-import { BaseSource } from "jsr:@shougo/ddu-vim@9.4.0/source";
+} from "jsr:@shougo/ddu-vim@9.5.0/types";
+import { BaseSource } from "jsr:@shougo/ddu-vim@9.5.0/source";
 import type { ActionData } from "jsr:@shougo/ddu-kind-file@0.9.0";
 
 type Params = {
