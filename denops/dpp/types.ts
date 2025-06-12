bf4942d9bbcbaa4d85a0d0ca7a88af424019a799
Shougo Matsushita <Shougo.Matsu@gmail.com>
2025-06-12 19:10:23 +0900

    Implement groups

--- denops/dpp/types.ts
+++ denops/dpp/types.ts
@@ -59,6 +59,7 @@ export type Plugin = {
   hook_source?: string;
   hooks_file?: string | string[];
   if?: boolean | string;
+  groups?: string[];
   lazy?: boolean;
   local?: boolean;
   lua_add?: string;
