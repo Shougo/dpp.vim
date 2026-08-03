d3206d783fab42ebbbcf2da7a7230d24d22b5c6a
Shougo Matsushita <Shougo.Matsu@gmail.com>
2026-08-03 09:36:17 +0900

    Improve printError()

--- denops/dpp/utils.ts
+++ denops/dpp/utils.ts
@@ -19,14 +19,22 @@ export async function printError(
 ) {
   const message = messages.map((v) => {
     if (v instanceof Error) {
-      // NOTE: In Deno, Prefer `Error.stack` because it contains `Error.message`.
+      // NOTE: In Deno, Prefer `Error.stack` because it contains
+      // `Error.message`.
       return `${v.stack ?? v}`;
-    } else if (typeof v === "object") {
-      return JSON.stringify(v);
-    } else {
-      return `${v}`;
     }
+
+    if (typeof v === "object" && v !== null) {
+      try {
+        return JSON.stringify(v);
+      } catch (_e: unknown) {
+        return String(v);
+      }
+    }
+
+    return String(v);
   }).join("\n");
+
   await denops.call("dpp#util#_error", message);
 }
 
