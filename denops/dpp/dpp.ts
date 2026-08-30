997734c7089e5f6a1c39402bbbc610f1f8e7d620
Shougo Matsushita <Shougo.Matsu@gmail.com>
2026-08-30 12:26:04 +0900

    Check README

--- denops/dpp/dpp.ts
+++ denops/dpp/dpp.ts
@@ -914,6 +914,14 @@ async function getRecordDocs(
   const recordDocs: Record<string, number> = {};
 
   for (const plugin of Object.values(recordPlugins)) {
+    for (const file of ["README.md", "README.mkd"]) {
+      const fullPath = `${plugin.rtp}/${file}`;
+      const stat = await safeStat(fullPath);
+        if (!stat) continue;
+
+        recordDocs[fullPath] = stat.size;
+    }
+
     for (const src of ["doc"]) {
       const srcDir = `${plugin.path}/${src}`;
       if (!plugin.path || !await isDirectory(srcDir)) {
